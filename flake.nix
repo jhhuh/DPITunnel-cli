cc8a34490f213c46851639ca8c5682dc7a2d8dd1
Ji-Haeng Huh <jhhuh.note@gmail.com>
2023-12-19 01:24:56 +0900

    add nix expression for the package

--- flake.nix
+++ flake.nix
@@ -16,47 +16,69 @@
 
       pkgs = import inputs.nixpkgs { inherit system; overlays = [ overlay ]; };
 
-      overlay = self: super:
-        {
-          DPITunnel-cli = self.callPackage ./. {};
+
+      mk-cpp-httplib = { stdenv, meson, ninja, openssl, brotli, zlib, pkg-config }:
+        stdenv.mkDerivation {
+          name = "cpp-httplib";
+          src = inputs.cpp-httplib;
+          nativeBuildInputs = [ meson ninja pkg-config ];
+          buildInputs = [ openssl brotli zlib ];
+          mesonFlags = ["-D cpp-httplib_compile=true"];
         };
 
-      cpp-httplib = pkgs.stdenv.mkDerivation {
-        name = "cpp-httplib";
-        src = inputs.cpp-httplib;
-        nativeBuildInputs = with pkgs; [ meson ninja pkg-config ];
-        buildInputs = with pkgs; [ openssl brotli zlib ];
-        mesonFlags = ["-D cpp-httplib_compile=true"];
-      };
+      mk-dnslib = { stdenv, cmake, pkg-config }:
+        stdenv.mkDerivation {
+          name = "dnslib";
+          #src = ./dnslib;
+          src = inputs.dnslib;
+          nativeBuildInputs = [ cmake pkg-config ];
+          env.cmakeDir = "../src/";
+          env.NIX_CFLAGS_COMPILE = "-Wno-error=unused-variable";
+          installPhase = ''
+            mkdir -p $out/lib/pkgconfig
+            mkdir -p $out/include/dnslib
 
-      dnslib = pkgs.stdenv.mkDerivation {
-        name = "dnslib";
-        #src = ./dnslib;
-        src = inputs.dnslib;
-        nativeBuildInputs = with pkgs; [ cmake pkg-config ];
-        env.cmakeDir = "../src/";
-        env.NIX_CFLAGS_COMPILE = "-Wno-error=unused-variable";
-        installPhase = ''
-          mkdir -p $out/lib/pkgconfig
-          mkdir -p $out/include/dnslib
+            cp ../src/*.h $out/include/dnslib/
+            cp ./libdnslib.a $out/lib/
 
-          cp ../src/*.h $out/include/dnslib/
-          cp ./libdnslib.a $out/lib/
+            cat >> $out/lib/pkgconfig/dnslib.pc << EOF
+            Name: dnslib
+            Description: dnslib
+            Version: 0.0.1
+            Libs: -L$out/lib/ -ldnslib
+            Cflags: -I$out/include
+            EOF
+          '';
+        };
 
-          cat >> $out/lib/pkgconfig/dnslib.pc << EOF
-          Name: dnslib
-          Description: dnslib
-          Version: 0.0.1
-          Libs: -L$out/lib/ -ldnslib
-          Cflags: -I$out/include
-          EOF
-        '';
-      };
+      mk-DPITunnel-cli = { stdenv, cmake, pkg-config, meson, libnl, openssl, cpp-httplib, dnslib }:
+        stdenv.mkDerivation {
+          name = "DPITunnel-cli";
+          src = ./.;
+          nativeBuildInputs = [ cmake pkg-config meson ];
+          buildInputs = [
+            libnl
+            openssl
+            cpp-httplib
+            dnslib
+          ];
+          installPhase = ''
+            mkdir -p $out/bin
+            cp ./DPITunnel-cli-exec $out/bin/DPITunnel-cli
+          '';
+        };
+
+      overlay = self: super:
+        {
+          DPITunnel-cli = self.callPackage mk-DPITunnel-cli {};
+          cpp-httplib = self.callPackage mk-cpp-httplib {};
+          dnslib = self.callPackage mk-dnslib {};
+        };
 
     in
       {
 
-        inherit inputs cpp-httplib dnslib;
+        packages.${system}.default = pkgs.DPITunnel-cli;
 
         devShells.${system}.default = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [ cmake pkg-config meson ninja python3 ];
